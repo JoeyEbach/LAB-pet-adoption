36e67566503af4102df3b8cea63a6af0cca94d5e
Joey Ebach <connect@joeyebach.com>
2023-07-18 06:19:13 -0500

    set up dynamic html cards on dom

--- main.js
+++ main.js
@@ -240,3 +240,28 @@ const pets = [
       imageUrl: "http://lsae2.iypcdn.com/static//modules/uploads/photos/language1/dino-live-22.jpg?119"
     }
   ];
+
+const targetingApp = document.querySelector("#app");
+
+let domString = "";
+for (const pet of pets) {
+  domString += `
+  <div class="card" style="width: 18rem;">
+    <img src="${pet.imageUrl}" class="card-img-top" alt="...">
+    <div class="card-body">
+    <h5 class="card-title">${pet.name}: ${pet.id}</h5>
+    <p>Type: ${pet.type}</p>
+    <p>Color: ${pet.color}</p>
+    <p>Special Skill: ${pet.specialSkill}</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>`
+}
+
+targetingApp.innerHTML = domString;
+
+console.log("I'm here!")
+
+// - color
+// - specialSkill
+// - typeOfPet
